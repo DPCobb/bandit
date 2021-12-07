228df513973785a0f6b4e07c6b6c214c0bc4d922
Dan Cobb <dancobb@gmail.com>
2021-12-07 00:16:31 -0500

    [feat]: exit cmd and unk error
    exit command and error and exit if method is not found

--- parser.py
+++ parser.py
@@ -20,11 +20,15 @@ class BanditParser:
     def parseCommand(self, cmd):
         # split 1 time this should give use the method
         parts = cmd.split(' ', 1)
+        args = '' if len(parts) < 2 else parts[1]
+        self.runCommand(parts[0], args)
 
-        self.runCommand(parts[0], parts[1])
-
-    def runCommand(self, method, args):
+    def runCommand(self, method, args=''):
         # These will be moved to methods, this is just quick test
+        if method not in self.methods:
+            print('Error! Unknown method: ' + method)
+            return 0
+
         if method == "EXEC":
             output = subprocess.run(args.split(' '))
             print(output)
@@ -32,3 +36,7 @@ class BanditParser:
         elif method == 'WAIT':
             wait = int(args)/1000
             time.sleep(wait)
+
+        elif method == "EXIT":
+            print("Script complete, exiting now.")
+            return 0
