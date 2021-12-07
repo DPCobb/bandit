970a9fe2675d28be296460b1b552fd9021089561
Dan Cobb <dancobb@gmail.com>
2021-12-07 00:28:44 -0500

    [feat]: title method
    moving title to method

--- bandit.py
+++ bandit.py
@@ -3,14 +3,7 @@ import os
 from parser import BanditParser
 
 
-@click.group()
-def main():
-    pass
-
-
-@main.command()
-def init():
-    """Title screen"""
+def showTitle():
     print("""
 .______        ___      .__   __.  _______   __  .___________.
 |   _  \      /   \     |  \ |  | |       \ |  | |           |
@@ -21,8 +14,17 @@ def init():
 
 Task Automation and GUI Action Scripts
 
-Use --help for available commands.
     """)
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def init():
+    showTitle()
 
 
 @main.command()
@@ -30,6 +32,8 @@ Use --help for available commands.
 @click.option('--verbose', '-v', is_flag=True, help="Turn on some more verbose output")
 def load(filename, verbose):
     """Load and run a Bandit file"""
+    showTitle()
+    print('Attempting to run .bdt file...')
     if (os.path.exists(filename)):
         print('File discovered, running script...')
         f = open(filename, 'r')
